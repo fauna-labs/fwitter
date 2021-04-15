a3b801738a9360fa825c36320aad85f17c7a5cae
Sergii Zinkevych <s.zinkevych@gmail.com>
2021-04-15 11:22:26 +0300

    feature: add x-fauna-source
    add header X-Fauna-Source when initializing faunadb.Client

--- src/fauna/query-manager.js
+++ src/fauna/query-manager.js
@@ -22,11 +22,14 @@ import { follow } from './queries/followers'
  */
 
 class QueryManager {
+  private headers = { 'X-Fauna-Source': 'fwitter-react' }
+
   constructor(token) {
     // A client is just a wrapper, it does not create a persitant connection
     // FaunaDB behaves like an API and will include the token on each request.
     this.bootstrapToken = token || process.env.REACT_APP_LOCAL___BOOTSTRAP_FAUNADB_KEY
     this.client = new faunadb.Client({
+      headers: this.headers,
       secret: token || this.bootstrapToken
     })
   }
@@ -34,7 +37,7 @@ class QueryManager {
   login(email, password) {
     return login(this.client, email, password).then(res => {
       if (res) {
-        this.client = new faunadb.Client({ secret: res.secret })
+        this.client = new faunadb.Client({ headers: this.headers, secret: res.secret })
       }
       return res
     })
@@ -45,7 +48,7 @@ class QueryManager {
     const icon = 'person' + (Math.round(Math.random() * 22) + 1)
     return registerWithUser(this.client, email, password, name, alias, icon).then(res => {
       if (res) {
-        this.client = new faunadb.Client({ secret: res.secret.secret })
+        this.client = new faunadb.Client({ headers: this.headers, secret: res.secret.secret })
       }
       return res
     })
@@ -54,6 +57,7 @@ class QueryManager {
   logout() {
     return logout(this.client).then(res => {
       this.client = new faunadb.Client({
+        headers: this.headers,
         secret: this.bootstrapToken
       })
       return res
